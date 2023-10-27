30011fd3353b4d2ca1502d90a4b28e8a1dc1899f
Yanka Fed <yankafiodorova@yandex.ru>
2023-10-28 02:36:58 +0300

    changes in configuration and yandexdriver were added

--- src/main/java/org/example/utils/WebDriverSettings.java
+++ src/main/java/org/example/utils/WebDriverSettings.java
@@ -1,6 +1,4 @@
-
 package org.example.utils;
-import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,28 +7,18 @@ public class WebDriverSettings {
 
 
         /**
-         На момент завершения работы над проектом, актуальный вебдрайвер для Яндекс браузера - 116, а ведрайвер для Chrome - 118.
-         По аналогии с настройкой из 4го спринта, получилось написать отдельный класс с настройкой последовательного запуска вебдрайверов
-         и открытия браузеров.
+         добавила яндексдрайвер и теперь нет локальных путей)
          */
         public static WebDriver createWebDriver(String browser) {
-            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver118");
             ChromeOptions options = new ChromeOptions();
-            options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
             options.addArguments("--remote-allow-origins=*");
-            options.setPageLoadStrategy(PageLoadStrategy.EAGER);
-            switch (browser) {
+            switch (browser){
                 case "chrome":
+                    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver118");
                     return new ChromeDriver(options);
-
                 case "yandex":
-                    System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver116");
-                    ChromeOptions secondOptions = new ChromeOptions();
-                    secondOptions.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex");
-                    secondOptions.addArguments("--remote-allow-origins=*");
-                    secondOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
-                    return new ChromeDriver(secondOptions.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex"));
-
+                    System.setProperty("webdriver.chrome.driver", "src/test/resources/yandexdriver");
+                    return new ChromeDriver(options);
                 default:
                     throw new RuntimeException("Incorrect browser name");
             }
